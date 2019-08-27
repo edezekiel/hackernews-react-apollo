8cdc943f125dad782980ad6e2f95309ff3dd8102
Edward Ezekiel <ed@ubiquisoft.com>
2019-08-27 14:14:45 -0500

    copy the Mutation from the demo

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -37,12 +37,10 @@ class CreateLink extends Component {
             placeholder="The URL for the link"
           />
         </div>
-        <Mutation mutation={POST_MUTATION variables={{ description, url }}>
-            {() => (
-            <button onClick={`... you'll implement this 🔜`}>
-                Submit
-            </button>
-            )}
+        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+          {() => (
+            <button onClick={`... you'll implement this 🔜`}>Submit</button>
+          )}
         </Mutation>
       </div>
     );
