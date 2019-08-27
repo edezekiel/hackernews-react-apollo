a081dc761aef5d0a0eec1c11327a91de1e53ad50
Edward Ezekiel <ed@ubiquisoft.com>
2019-08-27 14:23:57 -0500

    test the CreateLink component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/App.css';
 import LinkList from './LinkList'
+import CreateLink from './CreateLink'
 
 function App() {
   return (
-    <LinkList />
+    // <LinkList />
+    <CreateLink />
   );
 }
 
